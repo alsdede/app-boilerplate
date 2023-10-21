3c33dd82fdaff384d9ba16bdf8bdecfc7c24af33
Andre Silveira <als.andresilveira@gmail.com>
2023-10-21 17:40:05 +0100

    chore: add editorconfig eslint prettier

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    'plugins': [
+      ['module-resolver', {
+        'root': ['./src'],
+        'alias': {
+          '@assets': './assets',
+          '@components': './components',
+          '@routes':'./routes',
+          '@screens':'./screens',
+          '@services':'./services',
+          '@storage':'./storage',
+          '@utils':'./utils'
+        }
+      }]
+    ]
   };
 };
